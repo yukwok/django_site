e4a525ea41a211021d1c6dfe8c3ec96643098913
yukwok <billynyk@gmail.com>
2022-04-22 11:05:50 +0800

    render
    Use render instead of using HttpResponse.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
-
-
-def products(request):
-    return HttpResponse('<h1>Products page</h1>')
+    return render(request, 'blog/about.html')
